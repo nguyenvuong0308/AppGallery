99be930e8cbe57c0634bac9e0d0b04f50ed64f1f
=vuong.nguyen <=vuong.nguyen@ntq-solution.com.vn>
2019-03-19 18:04:06 +0700

    fix animation replace fragment

--- app/src/main/java/gallery/vnm/com/appgallery/Screen/MainActivity.java
+++ app/src/main/java/gallery/vnm/com/appgallery/Screen/MainActivity.java
@@ -92,7 +92,8 @@ public class MainActivity extends AppCompatActivity implements DrawerLayoutContr
 
     public void replaceFragment(Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.setCustomAnimations(R.anim.slide_in_from_right, R.anim.slide_out_to_left, R.anim.slide_in_from_left, R.anim.slide_out_to_right);
+//        transaction.setCustomAnimations(R.anim.slide_in_from_right, R.anim.slide_out_to_left, R.anim.slide_in_from_left, R.anim.slide_out_to_right);
+        transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out, android.R.anim.fade_in, android.R.anim.fade_out);
         transaction.replace(R.id.content_frame, fragment);
         transaction.addToBackStack(fragment.getClass().getSimpleName());
         transaction.commit();
